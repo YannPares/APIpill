bd67e11eccd2ea3c0d702e382f4217acaf5585be
Ian Pares <yannpares@gmail.com>
2023-08-01 13:40:40 +0200

    (fix) fetch fixed

--- posts.js
+++ posts.js
@@ -1,70 +1,82 @@
 
 const buttonPage = document.getElementById('btnPage')
-const myPosts = document.getElementById('myposts'); 
-let numPosts = 1;
+const myPosts = document.getElementById('post'); 
+const modalHeader = document.getElementById('modalHeader');
+const modalBody = document.getElementById('modalBody');
+let numPosts = 0;
+let numPost = 1;
 let buttonID = 1;
-buttonPage.addEventListener('click', getPage());
 
+buttonPage.addEventListener('click', getPage());
 function getPage(){ 
-    
-    url = `https://jsonplaceholder.typicode.com/posts/?_start=${numPosts}&_limit=10`;
-fetch (url)
-    .then (response => response.json())
-    
-    .then (data => {
+    fetch (`https://jsonplaceholder.typicode.com/posts/?_start=${numPosts}&_limit=8`)
+        .then (response => response.json())
         
-        // console.log(data)
-        myPosts.replaceChildren();
-        
-        data.forEach(post => {
-      
-        const li = document.createElement("li");
-        const btnOpen = document.createElement("button")
+        .then (data => {
+            myPosts.replaceChildren();
+            
+            data.forEach(post => {
+            const div = document.createElement("div");
+            const image = document.createElement("img")    
+            const btnOpen = document.createElement("button")
+            const titleModal = document.createElement("h3");
+            const bodyModal = document.createElement('p');
+            const userID = document.createElement('p');
+            modalHeader.replaceChildren();
+            modalBody.replaceChildren();
+            div.innerHTML = post.title;
+            div.setAttribute("class", "card");
+            myPosts.appendChild(div);
+
+            image.src = 'https://via.placeholder.com/200x200';
+            div.appendChild(image);
+
+            btnOpen.innerHTML = 'Open post';
+            btnOpen.setAttribute("id", `btnOpen${buttonID}`);
+            btnOpen.setAttribute("class", "btn btn-primary");
+            btnOpen.setAttribute("data-toggle","modal")
+            btnOpen.setAttribute("data-target", "#myModal")
+            div.appendChild(btnOpen);
+
+
+           
+            
+            titleModal.textContent = post.title;
+            modalHeader.appendChild(titleModal);
+
+            
+            bodyModal.innerHTML = post.body;
+            modalBody.appendChild(bodyModal);
+
+            userID.innerHTML = post.userId;
+            modalBody.appendChild(userID);
+
         console.log(post);
-        li.innerHTML = post.title;
-        myPosts.append(li);
-        btnOpen.innerHTML = 'Open post';
-        
-        btnOpen.setAttribute("id", `btnOpen${buttonID}`);
-        myPosts.appendChild(btnOpen);
-        buttonID += 1;
+            });
+
+        })
+        .catch(function (err) {
+            console.log(err);
         });
-   
-       
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
- numPosts += 10;
+     
+
+
+
+    // fetch (`https://jsonplaceholder.typicode.com/posts?id=${buttonID}?_body`)
+    //     .then (response2 => response2.json())
+    //     .then (data => {
+    //         data.forEach( ides => {
+                   
+            
+    //         console.log(ides);
+             
+    //         })
+    //         })   
+    //         .catch(function (err) {
+    //             console.log(err);
+    //         });
+            
+buttonID += 1;
+numPosts += 8;
 }
 
-// const openButton = document.getElementById('btnOpen');
-
-// openButton.addEventListener('click', getPost());
-// function getPost(){
-// fetch (urlpage)
-//     .then (response => response.json())
-    
-//     .then (data => {
-
-//         myPosts.replaceChildren();
-//         data.forEach(post => {
-//         const li = document.createElement("li");
-//         li.innerHTML = post.title + post.body + userID;
-//         myPosts.appendChild(li);
-//         const btnComm = document.createElement("button")
-//         btnComm.innerHTML = 'Show Comments';
-//         myPosts.appendChild(btnComm);
-//         const btnEdit = document.createElement("button");
-//         btnEdit.innerHTML = 'Edit post';
-//         myPosts.appendChild(btnEdit);
-//         const btnDelete = document.createElement("button");
-//         btnDelete.innerHTML = 'Delete post';
-//         myPosts.appendChild(btnDelete);
-
-//         });
-//     })
-//     .catch(function (err) {
-//         console.log(err);
-//     });
-// }
